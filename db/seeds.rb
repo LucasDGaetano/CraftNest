4c0ce2560e83d6de181b72ff61a1601b4716c2ba
Lucas De Gaetano <lucasdgpro@gmail.com>
2023-11-20 20:14:44 +0100

    resolved conflict of last pull request

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,9 @@ user3 = User.create!(username: "xavierdupont", email: "xavier.dupont@yahoo.fr", 
 user4 = User.create!(username: "charlesdonatelo", email: "donatelo@yahoo.fr", password: "charlesdon")
 user5 = User.create!(username:"macarena", email:"macarena1206@yahoo.fr", password:"brazilia12")
 user6 = User.create!(username:"soprano12", email:"courdechant@hotmail.com", password:"beethoven12")
-user8 = User.create!(username:"peinturedeparis", email:"peintureparis@hotmail.fr", password:"123456")
-user9 = User.create!(username:"mozart", email:"mozart@hotmail.fr", password:"123456")
-user10 = User.create!(username:"beethoven", email:"beethoven@hotmail.fr", password:"123456")
+user8 = User.create!(username:"peinturedeparis", email:"peintureparis@hotmail.fr", password: "123456")
+user9 = User.create!(username:"mozart", email:"mozart@hotmail.fr", password: "123456")
+user10 = User.create!(username:"beethoven", email:"beethoven@hotmail.fr", password: "123456")
 puts "CREATED USERS"
 
 atelier1 = Atelier.create!(name: "Ateliernumero1", category: "peinture", location: "Paris", price: 300, user: user1)
@@ -34,7 +34,7 @@ atelier3 = Atelier.create!(name: "Ateliernumero3", category: "sculpture", locati
 atelier4 = Atelier.create!(name: "Ateliernumero4", category: "joaillerie", location: "LILLE", price: 220, user: user4)
 puts "CREATED ATELIERS"
 
-resa1 = Reservation.create!(atelier: atelier1, user: user4, start_date: Date.new(2023, 11, 25), end_date: Date.new(2023, 11, 26))
+resa1 = Reservation.create!(atelier: atelier1, user: user4, start_date: Date.new(), end_date: 25/11/2023)
 
 # puts 'Liste ateliers'
 # atelier_d_argent = Atelier.new(name: "Atelier d'Argent", location: "Paris", price: 300, category: "joaillerie")
