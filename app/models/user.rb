2574d8e0e659e4504fe62fd5922c3573887b2618
zaira cosman <zairacosman@yahoo.fr>
2023-11-20 16:14:34 +0100

    validation user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+has_many :reservations
+has_many :ateliers
+has_many :reviews
+
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+validates :email, presence: true, length: { minimum: 10, maximum: 30 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
+validates :username, presence: true
+validates :password, presence: true, length: { minimum: 6, maximum: 15 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
