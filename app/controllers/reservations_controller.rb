84d59ef89c5b119c4da51eb5b772888340448551
Valentin Bailly <valentinbailly8@gmail.com>
2023-11-20 15:57:00 +0100

    Validates model

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,9 @@
 class ReservationsController < ApplicationController
+
+  def create
+  end
+
+  def destroy
+    
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,22 @@ class Reservation < ApplicationRecord
   belongs_to :atelier
   belongs_to :user
   belongs_to :review
+
+  validates :atelier_id, presence: true
+  validates :user_id, presence: true
+  validates :review_id, presence: true
+  validates :start_date, presence: true
+  validates :start_date, availability: true
+  validates :end_date, presence: true
+  validates :end_date, availability: true
+
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    if end_date <= start_date
+      errors.add(:end_date, "Must be after the start date!")
+    end
+  end
 end
