2fc3d6a42152e03122158c92570a3022a9445c86
zaira cosman <zairacosman@yahoo.fr>
2023-11-21 15:49:43 +0100

    profile wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def dashboard
+    # @reservations = current_user.reservations
+    @user = current_user
+  end
 end
