b27cdabbdc8014718e67feb0a2ff6eac1a07445f
Valentin Bailly <valentinbailly8@gmail.com>
2023-11-22 16:43:10 +0100

    Localisations faites

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,52 +31,52 @@ user10 = User.create!(username: "ArthurGumery", email: "arthur.gumery@gmail.com"
 puts "CREATED USERS"
 
 file1 = URI.open("https://www.pariszigzag.fr/wp-content/uploads/2019/03/atelier-suzanne-valandon-paris-zigag-e1551883982843.jpg")
-atelier1 = Atelier.create!(name: "Au plaisir du petit Paris", category: "Peinture", location: "Paris", price: 300, user: user1)
+atelier1 = Atelier.new(name: "Au plaisir du petit Paris", category: "Peinture", location: "Paris", price: 300, user: user1)
 atelier1.photo.attach(io: file1, filename: "atelier.png", content_type: "image/png")
 atelier1.save!
 
 file2 = URI.open("https://resize-elle.ladmedia.fr/rcrop/638,,forcex/img/var/plain_site/storage/images/deco/art-decoration/ateliers-d-artistes-des-lieux-inspires/69480487-1-fre-FR/Ateliers-d-artistes-des-lieux-inspires.jpg")
-atelier2 = Atelier.create!(name: "Au coin du feu", category: "Sculpture", location: "Poitiers", price: 120, user: user2)
+atelier2 = Atelier.new(name: "Au coin du feu", category: "Sculpture", location: "Poitiers", price: 120, user: user2)
 atelier2.photo.attach(io: file2, filename: "atelier2.png", content_type: "image/png")
 atelier2.save!
 
 file3 = URI.open("https://www.tiaramilo.com/build/images/static_content/joaillerie/atelier-joaillerie-bague-flamme-soudure.jpg")
-atelier3 = Atelier.create!(name: "Le doux repos", category: "Joaillerie", location: "Lyon", price: 150, user: user3)
+atelier3 = Atelier.new(name: "Le doux repos", category: "Joaillerie", location: "Lyon", price: 150, user: user3)
 atelier3.photo.attach(io: file3, filename: "atelier3.png", content_type: "image/png")
 atelier3.save!
 
 file4 = URI.open("https://atelierlapatente.org/wp/wp-content/uploads/2020/05/Panorama.jpg")
-atelier4 = Atelier.create!(name: "Premier mercredi", category: "Bois", location: "Lille", price: 220, user: user4)
+atelier4 = Atelier.new(name: "Premier mercredi", category: "Bois", location: "Lille", price: 220, user: user4)
 atelier4.photo.attach(io: file4, filename: "atelier4.png", content_type: "image/png")
 atelier4.save!
 
 file5 = URI.open("https://www.arielledegasquet.com/wp-content/uploads/2017/03/170913__Arielle-de-Gasquet-atelier-ceramique__DSF8623.jpg")
-atelier5 = Atelier.create!(name: "Le four", category: "Céramique", location: "Rennes", price: 400, user: user5)
+atelier5 = Atelier.new(name: "Le four", category: "Céramique", location: "Rennes", price: 400, user: user5)
 atelier5.photo.attach(io: file5, filename: "atelier5.png", content_type: "image/png")
 atelier5.save!
 
 file6 = URI.open("https://photos.infolocale.fr/infoLocale/annonce/2022/0824/7648404/_1.jpg")
-atelier6 = Atelier.create!(name: "Tu m'as vu", category: "Peinture", location: "Angers", price: 100, user: user6)
+atelier6 = Atelier.new(name: "Tu m'as vu", category: "Peinture", location: "Angers", price: 100, user: user6)
 atelier6.photo.attach(io: file6, filename: "atelier6.png", content_type: "image/png")
 atelier6.save!
 
 file7 = URI.open("https://www.infrarouge.fr/wp-content/uploads/2017/10/Messika-immersion-joaillerie-Euge%CC%81nie-Martinez-011.jpg")
-atelier7 = Atelier.create!(name: "Chère enfant", category: "Joaillerie", location: "Compiègne", price: 500, user: user7)
+atelier7 = Atelier.new(name: "Chère enfant", category: "Joaillerie", location: "Compiègne", price: 500, user: user7)
 atelier7.photo.attach(io: file7, filename: "atelier7.png", content_type: "image/png")
 atelier7.save!
 
 file8 = URI.open("https://www.apeidieppe.fr/msmedias/ateliers-etran/atelier-bois-solidaire/atelier-4.jpg")
-atelier8 = Atelier.create!(name: "A la joie de Bagnolet", category: "Bois", location: "Bagnolet", price: 600, user: user8)
+atelier8 = Atelier.new(name: "A la joie de Bagnolet", category: "Bois", location: "Bagnolet", price: 600, user: user8)
 atelier8.photo.attach(io: file8, filename: "atelier8.png", content_type: "image/png")
 atelier8.save!
 
 file9 = URI.open("https://i2.wp.com/neo-ceramistes.com/wp-content/uploads/2020/02/atelier-ceramique-delft.jpg?fit=800%2C521&ssl=1")
-atelier9 = Atelier.create!(name: "A côté du marché", category: "Céramique", location: "Reims", price: 200, user: user9)
+atelier9 = Atelier.new(name: "A côté du marché", category: "Céramique", location: "Reims", price: 200, user: user9)
 atelier9.photo.attach(io: file9, filename: "atelier9.png", content_type: "image/png")
 atelier9.save!
 
 file10 = URI.open("https://images.squarespace-cdn.com/content/v1/61cca7fa29f52d677c682513/65fdd3af-bae9-4c1e-9c07-d6ecf0fe6a1a/La+Fabrique+du+Canal+Atelier+c%C3%A9ramique+cours+poterie+Paris+19")
-atelier10 = Atelier.create!(name: "La bonne chaleur", category: "Céramique", location: "Marseille", price: 340, user: user10)
+atelier10 = Atelier.new(name: "La bonne chaleur", category: "Céramique", location: "Marseille", price: 340, user: user10)
 atelier10.photo.attach(io: file10, filename: "atelier10.png", content_type: "image/png")
 atelier10.save!
 
